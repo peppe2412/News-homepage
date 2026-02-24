74eb82e30d1a4bccb83b6a921d5b054a7c274a25
peppe2412 <peppeterrazzano79@gmail.com>
2026-02-24 15:44:00 +0100

    Change sidebar code

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const sidebar = document.querySelector("#sidebar");
+const open_sidebar = document.querySelector("#open-sidebar")
+const close_sidebar = document.querySelector("#close-sidebar")
 const overlay = document.querySelector("#overlay");
 const main = document.querySelector("main");
 
@@ -12,6 +14,9 @@ if (storage === "close") {
   closeSidebar();
 }
 
+open_sidebar.addEventListener("click", openSidebar)
+close_sidebar.addEventListener("click", closeSidebar)
+
 window.addEventListener("resize", ()=>{
     if(window.innerWidth >= 1024){
         closeSidebar()
@@ -22,6 +27,7 @@ function openSidebar() {
   localStorage.setItem("sidebar", "open");
   sidebar.classList.remove("hidden");
   overlay.classList.remove("hidden");
+  document.body.style.overflowY = "hidden"
   main.inert = true;
 }
 
@@ -29,5 +35,6 @@ function closeSidebar() {
   localStorage.setItem("sidebar", "close");
   sidebar.classList.add("hidden");
   overlay.classList.add("hidden");
+  document.body.style.overflowY = "scroll"
   main.inert = false;
 }
